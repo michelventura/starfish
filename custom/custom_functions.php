dd2ee933ae369614f5f2c50e9bb74976becad864
Mich <pmichven@gmail.com>
2016-08-15 17:23:36 -0700

    Custom footer credits

--- custom/custom_functions.php
+++ custom/custom_functions.php
@@ -4,3 +4,15 @@
 
 //* Remove the edit link
 add_filter ( 'genesis_edit_post_link' , '__return_false' );
+
+//* Allow shortcodes to execute in widget areas
+add_filter('widget_text', 'do_shortcode');
+
+//* Customize the footer credits
+add_filter( 'genesis_footer_creds_text', 'my_custom_footer_creds' );
+function my_custom_footer_creds(){
+
+    $creds = '[footer_copyright] <a href="http://starfish.dev/">Starfish VIP</a>';
+    return $creds;
+
+}
